a76f11452b7bc6d39abede88926fb24bbc383b66
André Reis <reis.andre20@gmail.com>
2016-09-01 22:09:48 +0100

    change server

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-var http = require('http')
-var url = require('url')
-var fs = require('fs')
-var path = require('path')
 
-http.createServer(function (pedido, resposta) {
- 
-// Aqui vamos escrever o código do servidor que vai ser
-// executado sempre que for feito um pedido
- var caminho = url.parse(pedido.url).pathname;
- if (caminho==='/') {
- var ficheiro = path.join(__dirname, 'public', caminho, 'index.html');
-} else {
- var ficheiro = path.join(__dirname, 'public', caminho);
-}
 
-fs.readFile(ficheiro, function (erro, dados) {
-if (erro) {
-resposta.writeHead(404);
-resposta.end();
-} else {
-resposta.end(dados);
-}
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/public');
+
+app.get('/', function(request, response) {
+  response.render('index');
 });
 
-
-
- 
-}).listen(8081, 'localhost', function () {
-  console.log('--- O servidor arrancou ---');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
